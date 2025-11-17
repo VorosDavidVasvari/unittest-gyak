e2212d3344228801577d300e5a001a7e93b3e6f0
Vörös Dávid <c2vordav@vasvari.org>
2025-11-17 09:03:32 +0100

    Wrote the program

---
+++ App.py
@@ -0,0 +1,13 @@
+def fibo(n: int) -> int:
+    if n < 0: return -1
+    elif n == 0: return 0
+
+    a = 0
+    b = 1
+
+    for _ in range(n - 1):
+        c = a + b
+        a = b
+        b = c
+
+    return b
